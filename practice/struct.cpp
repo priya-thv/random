1b338067bc31a39969f8d0563be1aec55c413312
priya <107272239+priya-thv@users.noreply.github.com>
2022-10-18 11:40:37 +0530

    Create struct.cpp

---
+++ practice/struct.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct student{
+    int age;
+    int marks;
+    string name;
+};
+
+int main(){
+    
+    student s;
+    s.age=20;
+    s.name="priya";
+    cout<<s.age<<endl;
+    cout<<s.name<<endl;
+    
+}
